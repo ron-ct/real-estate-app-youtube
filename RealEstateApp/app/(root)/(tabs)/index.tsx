b181bbc72a650946414296e47728fa6022ad737e
ron-ct <ronkaranja@outlook.com>
2025-02-17 13:01:01 +0300

    setup native wind

--- RealEstateApp/app/(root)/(tabs)/index.tsx
+++ RealEstateApp/app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
       <Link href="/sign-in">SignIn</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
---
+++ RealEstateApp/app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+const Profile = () =>{
+    return (
+    <View>
+        <Text>Wilkommen</Text>
+    </View>
+        
+    )
+}
+
+export default Profile;
